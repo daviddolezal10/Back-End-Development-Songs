f22a9ca416d36007d0724d9771c6fdad445f7151
daviddolezal10 <dolezda1@uhk.cz>
2024-04-26 05:15:41 -0400

    implemented songs service

--- backend/routes.py
+++ backend/routes.py
@@ -51,3 +51,78 @@ def parse_json(data):
 ######################################################################
 # INSERT CODE HERE
 ######################################################################
+@app.route("/health", methods=["GET"])
+def health_check():
+    return {"status": "OK"}
+
+@app.route("/count", methods=["GET"])
+def count_documents():
+    count = db.songs.count_documents({})
+    return {"count": count}, 200
+
+@app.route("/song", methods=["GET"])
+def get_songs():
+    # Vyhledání všech písní v kolekci 'songs'
+    all_songs = db.songs.find({})
+    # Převod na seznam
+    songs_list = [parse_json(song) for song in all_songs]
+    # Vrácení seznamu písní s kódem 200
+    return {"songs": songs_list}, 200
+
+@app.route("/song/<int:id>", methods=["GET"])
+def get_song_by_id(id):
+    # Vyhledání písně podle ID
+    song = db.songs.find_one({"id": id})
+    if song:
+        # Pokud je píseň nalezena, vrátí ji jako JSON s kódem 200
+        return parse_json(song), 200
+    else:
+        # Pokud není nalezena, vrátí zprávu s kódem 404
+        return {"message": "song with id not found"}, 404
+
+@app.route("/song", methods=["POST"])
+def create_song():
+    # Extrahování dat z těla požadavku
+    song_data = request.get_json()
+
+    # Zjištění, zda již existuje píseň s tímto ID
+    existing_song = db.songs.find_one({"id": song_data["id"]})
+
+    if existing_song:
+        # Pokud píseň již existuje, vrátí chybovou zprávu s kódem 302
+        return {"Message": f"song with id {song_data['id']} already present"}, 302
+
+    # Pokud ne, vloží novou píseň do databáze
+    result = db.songs.insert_one(song_data)
+
+    # Vrátí ID nově vložené písně s kódem 201
+    return {"inserted id": str(result.inserted_id)}, 201
+
+@app.route("/song/<int:id>", methods=["PUT"])
+def update_song(id):
+    # Extrahování dat písně z těla požadavku
+    new_data = request.json
+    
+    # Vyhledání písně podle ID
+    song = db.songs.find_one({"id": id})
+    
+    if song:
+        # Pokud je píseň nalezena, aktualizujte ji
+        db.songs.update_one({"id": id}, {"$set": new_data})
+        updated_song = db.songs.find_one({"id": id})
+        return parse_json(updated_song), 200
+    else:
+        # Pokud není píseň nalezena, vrátí zprávu s kódem 404
+        return {"message": "song not found"}, 404
+
+@app.route("/song/<int:id>", methods=["DELETE"])
+def delete_song(id):
+    # Odstranění písně podle ID
+    result = db.songs.delete_one({"id": id})
+
+    if result.deleted_count == 0:
+        # Pokud není nalezena píseň, vrátí 404
+        return {"message": "song not found"}, 404
+    else:
+        # Pokud je píseň smazána, vrátí 204 bez obsahu
+        return '', 204
